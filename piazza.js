6474c9ef14170dcf610834125b2264dc767e5c6a
Joel Choo <joelchoo@gmail.com>
2013-09-14 17:02:59 -0400

    added in background colours

--- piazza.js
+++ piazza.js
@@ -6,5 +6,6 @@ else {
 }
 $(window).load(function () {
 	alert("it claims it's done");
+	$('a').css({'background-color': 'yellow'});
 	$('.mark_unread_action').append('<li class="testing" onclick="history.back();">go back</li>');
 });
