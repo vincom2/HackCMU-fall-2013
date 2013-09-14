3805781299131c2a7c6a1b072e0c3cad5afba777
Vincent Huang <vincom2@gmail.com>
2013-09-14 19:54:29 -0400

    terrible, terrible hacks - see comments in committed file for details

--- piazza.js
+++ piazza.js
@@ -1,12 +1,10 @@
-if(jQuery) {
-	alert("hello world!");
-}
-else {
-	alert("fuck!");
-}
+// we would instead like to append stuff that saves the post to favourites
 $toappend = '<li class="testing" onclick="history.back();">go back</li>';
 
 $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
+	//dreadful hack: since we can't get the "add only to one place" thing to work,
+	//we'll just extract post ID and add shit *everywhere* and not care, because nobody can mouseover
+	//multiple posts at once. just make sure to extract the post ID so we can save the appropriate post.
+	$post_id = this.id.split(/_/)[1];
 	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
-	$('<ul>').appendTo('[id^='+this.id.split(/_/)[1]+']').append($toappend);
 });
