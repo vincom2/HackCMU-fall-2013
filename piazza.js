0b10db9f0d6bb3eb3b10a9e1eb5aa3ec24547b54
Vincent Huang <vincom2@gmail.com>
2013-09-14 19:40:24 -0400

    now it only adds to the item being mouse-overed, but clearly it's appending to the wrong place...

--- piazza.js
+++ piazza.js
@@ -7,6 +7,7 @@ else {
 $toappend = '<li class="testing" onclick="history.back();">go back</li>';
 
 $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
-	alert(e.target.id);
-	$('<ul>').appendTo('[id^='+e.target.id+']').append($toappend);
+	alert(this.id);
+	alert(this.id.split(/_/)[1]);
+	$('<ul>').appendTo('[id^='+this.id.split(/_/)[1]+']').append($toappend); //get the numeric part only
 });
