570d85e85568362a6b22fe3f4a3cca5bec46136e
Vincent Huang <vincom2@gmail.com>
2013-09-14 18:04:39 -0400

    choice comments added

--- piazza.js
+++ piazza.js
@@ -4,15 +4,20 @@ if(jQuery) {
 else {
 	alert("fuck!");
 }
-$toappend= '<li class="testing" onclick="history.back();">go back</li>';
 
-$(document).ready(function () {
-	alert("it claims it's done");
+// $(document).ready(function () {
+// 	alert("it claims it's done");
+// 	$('a').css({'background-color': 'yellow'});
+
+// 	$('.mark_unread_action').append($toappend);
+// });
+
+// $('#FeedItemDropdown*').mouseover(function() {
+// 	$('.mark_unread_action').append($toappend);
+// })
+
+setTimeout(function () {
+	alert("lulz");
 	$('a').css({'background-color': 'yellow'});
-
-	$('.mark_unread_action').append($toappend);
-});
-
-$('#FeedItemDropdown*').mouseover(function() {
-	$('.mark_unread_action').append($toappend);
-})
+	$('.mark_unread_action').append('<li class="testing" onclick="history.back();">go back</li>'); //it just hates this for some reason
+}, 5000);
