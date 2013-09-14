5f44b94bcb9c670bdf5b8f1edd153d99e681939e
Vincent Huang <vincom2@gmail.com>
2013-09-14 19:23:54 -0400

    WHY CAN'T I SELECT BY EVENT THING ID

--- piazza.js
+++ piazza.js
@@ -6,31 +6,7 @@ else {
 }
 $toappend = '<li class="testing" onclick="history.back();">go back</li>';
 
-// $(document).ready(function () {
-// 	alert("it claims it's done");
-// 	$('a').css({'background-color': 'yellow'});
-
-// 	$('.mark_unread_action').append($toappend);
-// });
-
-// $('#FeedItemDropdown*').mouseover(function() {
-// 	$('a').css({'background-color': 'yellow'});
-// 	$('.mark_unread_action').append($toappend);
-// })
-
-// setTimeout(function () {
-// 	alert("lulz");
-// 	$('a').css({'background-color': 'yellow'});
-// 	$('.mark_unread_action').append('<li class="testing" onclick="history.back();">go back</li>'); //it just hates this for some reason
-// }, 5000);
-
-$(document).on('mouseenter', '[id^=FeedItemDropdown]', function() {
-	$('a').css({'background-color': 'yellow'});
-	// $('[class=mark_unread_action]').append($toappend); //THIS JUST WON'T WORK
-	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
-});
-$(document).on('mouseleave', '[id^=FeedItemDropdown]', function() {
-	$('a').css({'background-color': 'yellow'});
-	// $('[class=mark_unread_action]').append($toappend);
-	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
+$(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
+	alert(e.target.id);
+	$('<ul>').appendTo('[id^='+e.target.id+']').append($toappend);
 });
