062c83ec0a1adac7c6178ab8a59da66c0fb28f95
Vincent Huang <vincom2@gmail.com>
2013-09-14 18:17:33 -0400

    mouseover now works, but the append code STILL WON'T WORK

--- piazza.js
+++ piazza.js
@@ -4,6 +4,7 @@ if(jQuery) {
 else {
 	alert("fuck!");
 }
+$toappend = '<li class="testing" onclick="history.back();">go back</li>';
 
 // $(document).ready(function () {
 // 	alert("it claims it's done");
@@ -13,11 +14,21 @@ else {
 // });
 
 // $('#FeedItemDropdown*').mouseover(function() {
+// 	$('a').css({'background-color': 'yellow'});
 // 	$('.mark_unread_action').append($toappend);
 // })
 
-setTimeout(function () {
-	alert("lulz");
+// setTimeout(function () {
+// 	alert("lulz");
+// 	$('a').css({'background-color': 'yellow'});
+// 	$('.mark_unread_action').append('<li class="testing" onclick="history.back();">go back</li>'); //it just hates this for some reason
+// }, 5000);
+
+$(document).on('mouseenter', '[id^=FeedItemDropdown]', function() {
 	$('a').css({'background-color': 'yellow'});
-	$('.mark_unread_action').append('<li class="testing" onclick="history.back();">go back</li>'); //it just hates this for some reason
-}, 5000);
+	$('[class=mark_unread_action]').append($toappend); //THIS JUST WON'T WORK
+});
+$(document).on('mouseleave', '[id^=FeedItemDropdown]', function() {
+	$('a').css({'background-color': 'yellow'});
+	$('[class=mark_unread_action]').append($toappend);
+});
