62d0e8bed6afce404b85ae07c4ddf2d05403f581
Vincent Huang <vincom2@gmail.com>
2013-09-14 16:54:13 -0400

    use $(window).load

--- piazza.js
+++ piazza.js
@@ -1,2 +1,10 @@
-alert("hello world!");
-$('.feed_item_dropdown_arrow ul').append('<li class="testing" onclick="history.back();">go back</li>');
+if(jQuery) {
+	alert("hello world!");
+}
+else {
+	alert("fuck!");
+}
+$(window).load(function () {
+	alert("it claims it's done");
+	$('.mark_unread_action').append('<li class="testing" onclick="history.back();">go back</li>');
+});
