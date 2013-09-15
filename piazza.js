78a39cc563bad38aece80437c6b744cd09949dcc
Vincent Huang <vincom2@gmail.com>
2013-09-14 21:23:42 -0400

    piazza.js is supposedly successfully saving things to chrome local storage

--- piazza.js
+++ piazza.js
@@ -1,3 +1,7 @@
+$.getScript(chrome.extension.getURL("savestuff.js"), function() {
+	console.log('savestuff.js successfully loaded in piazza.js');
+});
+
 $class_url = $(location).attr('href');
 
 $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
@@ -5,10 +9,12 @@ $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
 	//we'll just extract post ID and add shit *everywhere* and not care, because nobody can mouseover
 	//multiple posts at once. just make sure to extract the post ID so we can save the appropriate post.
 	$num_string = $('#'+this.id).children('ul').find('.disabled').text();
-	$post_num = $num_string.split(/ /)[1];
-	'<li class="testing" onclick="history.back();">go back</li>';
+	$post_num = ($num_string.split(/ /)[1]).split(/@/)[1];
+
 	// we would instead like to append stuff that saves the post to favourites
-	$toappend = '<li class="testing" onclick="location.href=\'' + $class_url + '?cid=' + $post_num.split(/@/)[1] + '\';">view post</li>';
-	// alert($toappend);
+	// $toappend = '<li class="testing" onclick="location.href=\'' + $class_url + '?cid=' + $post_num.split(/@/)[1] + '\';">view post</li>';
+	$post_link = $class_url + '?cid=' + $post_num
+	$toappend = '<li class="testing" onclick=storePost('+$post_num+',"'+$post_link+'");>Save this post';
+	alert($toappend);
 	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
 });
---
+++ savestuff.js
@@ -0,0 +1,32 @@
+function storePost(id, link) {
+	try {
+		console.log("Storing [" + id + ":" + link + "]");
+		window.localStorage.removeItem(id);      // <-- Local storage!
+     	window.localStorage.setItem(id, link);  // <-- Local storage!
+	} catch(e) {
+      console.log("Error inside setItem");
+      console.log(e);
+    }
+    console.log("Return from setItem" + id + ":" + link);
+  }
+
+function getItem(key) {
+    var value;
+    console.log('Retrieving key [' + key + ']');
+    try {
+      value = window.localStorage.getItem(key);  // <-- Local storage!
+    }catch(e) {
+      console.log("Error inside getItem() for key:" + key);
+      console.log(e);
+      value = "null";
+    }
+    console.log("Returning value: " + value);
+    return value;
+  }
+
+  // Clears all key/value pairs in local storage.
+  function clearStrg() {
+    console.log('about to clear local storage');
+    window.localStorage.clear(); // <-- Local storage!
+    console.log('cleared');
+  }
