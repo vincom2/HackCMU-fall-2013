a79bd39735454da142b42652050983fa8271ac11
Vincent Huang <vincom2@gmail.com>
2013-09-14 20:41:59 -0400

    IT'S CLICKABLE AND GETS POST NUMBER

--- piazza.js
+++ piazza.js
@@ -1,16 +1,14 @@
 $class_url = $(location).attr('href');
-// we would instead like to append stuff that saves the post to favourites
-$toappend = '<li class="testing" onclick="history.back();">go back</li>';
 
 $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
 	//dreadful hack: since we can't get the "add only to one place" thing to work,
 	//we'll just extract post ID and add shit *everywhere* and not care, because nobody can mouseover
 	//multiple posts at once. just make sure to extract the post ID so we can save the appropriate post.
-	// $post_id = this.id.split(/_/)[1]; //maybe we don't want to split after all LOL
-	//because the post ID (as in the @num one) appears to be under the FeedItemDropdown_ part anyway
-	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
-	// $the_string = $('#'+this.id).children('ul li').find('.disabled').text();
 	$num_string = $('#'+this.id).children('ul').find('.disabled').text();
 	$post_num = $num_string.split(/ /)[1];
-	alert($post_num);
+	'<li class="testing" onclick="history.back();">go back</li>';
+	// we would instead like to append stuff that saves the post to favourites
+	$toappend = '<li class="testing" onclick="location.href=\'' + $class_url + '?cid=' + $post_num.split(/@/)[1] + '\';">view post</li>';
+	// alert($toappend);
+	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
 });
