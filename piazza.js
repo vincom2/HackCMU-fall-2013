78a133237e43647c9db349ed2ce6af684b7800f1
Vincent Huang <vincom2@gmail.com>
2013-09-14 20:29:18 -0400

    extracts @post_number

--- piazza.js
+++ piazza.js
@@ -6,7 +6,11 @@ $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
 	//dreadful hack: since we can't get the "add only to one place" thing to work,
 	//we'll just extract post ID and add shit *everywhere* and not care, because nobody can mouseover
 	//multiple posts at once. just make sure to extract the post ID so we can save the appropriate post.
-	$post_id = this.id.split(/_/)[1]; //maybe we don't want to split after all LOL
+	// $post_id = this.id.split(/_/)[1]; //maybe we don't want to split after all LOL
 	//because the post ID (as in the @num one) appears to be under the FeedItemDropdown_ part anyway
 	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
+	// $the_string = $('#'+this.id).children('ul li').find('.disabled').text();
+	$num_string = $('#'+this.id).children('ul').find('.disabled').text();
+	$post_num = $num_string.split(/ /)[1];
+	alert($post_num);
 });
