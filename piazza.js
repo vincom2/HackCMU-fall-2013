81bf33e15aefb0ca6ddbc9edb5ed6162f4e90448
Vincent Huang <vincom2@gmail.com>
2013-09-14 20:10:38 -0400

    verbose comments on extracting @num

--- piazza.js
+++ piazza.js
@@ -1,3 +1,4 @@
+$class_url = $(location).attr('href');
 // we would instead like to append stuff that saves the post to favourites
 $toappend = '<li class="testing" onclick="history.back();">go back</li>';
 
@@ -5,6 +6,7 @@ $(document).on('mouseenter mouseleave', '[id^=FeedItemDropdown]', function(e) {
 	//dreadful hack: since we can't get the "add only to one place" thing to work,
 	//we'll just extract post ID and add shit *everywhere* and not care, because nobody can mouseover
 	//multiple posts at once. just make sure to extract the post ID so we can save the appropriate post.
-	$post_id = this.id.split(/_/)[1];
+	$post_id = this.id.split(/_/)[1]; //maybe we don't want to split after all LOL
+	//because the post ID (as in the @num one) appears to be under the FeedItemDropdown_ part anyway
 	$('<ul>').appendTo('[id^=FeedItemDropdown]').append($toappend);
 });
